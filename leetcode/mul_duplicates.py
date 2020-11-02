9d3baecb49b517ad469f775497611608aaa4aa4c
Mike Czech <mail@mczech.de>
2020-11-02 13:20:06 +0100

    Update duplicates puzzle (O(1) mem)

--- leetcode/mul_duplicates.py
+++ leetcode/mul_duplicates.py
@@ -1,14 +1,14 @@
 from typing import List
 
-def findDuplicates(nums: List[int]) -> List[int]:
-    ret = []
-    for i in range(len(nums)):
-        index = nums[i] - 1
-        breakpoint()
-        if nums[index] < 0:
-            ret.append(abs(nums[i]))
-        else:
-            nums[index] *= -1
-    return ret
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        ret = []
+        for i in range(len(nums)):
+            index = abs(nums[i]) - 1
+            if nums[index] < 0:
+                ret.append(abs(nums[i]))
+            else:
+                nums[index] *= -1
+        return ret
 
-findDuplicates([2, 2])
+Solution().findDuplicates([2, 2])
