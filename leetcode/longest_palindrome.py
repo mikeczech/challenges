4fa96cd93c98b19d2eaf164409f9e6c9c0fde13c
Mike Czech <mail@mczech.de>
2020-11-02 13:23:56 +0100

    Update solution

--- leetcode/longest_palindrome.py
+++ leetcode/longest_palindrome.py
@@ -1,21 +1,21 @@
 import operator
 
-def longest_palindrom(s: str):
-    cache = set(s)
-    for j in range(0, len(s) - 2 + 1):
-        subs = s[j:j+2]
-        if subs[0] == subs[-1] and subs not in cache:
-            cache.add(subs)
-
-    for i in range(3, len(s) + 1):
-        for j in range(0, len(s) - i + 1):
-            subs = s[j:j+i]
-            print(subs)
-            if subs[0] == subs[-1] and subs[1:-1] in cache:
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        cache = set(s)
+        for j in range(0, len(s) - 2 + 1):
+            subs = s[j:j+2]
+            if subs[0] == subs[-1]:
                 cache.add(subs)
-    return max(cache, key=len)
 
-assert longest_palindrom("babad") == "aba"
-assert longest_palindrom("cbbd") == "bb"
-assert longest_palindrom("aba") == "aba"
-assert longest_palindrom("ccc") == "ccc"
+        for i in range(3, len(s) + 1):
+            for j in range(0, len(s) - i + 1):
+                subs = s[j:j+i]
+                if subs[0] == subs[-1] and subs[1:-1] in cache:
+                    cache.add(subs)
+        return max(cache, key=len)
+
+assert Solution().longest_palindrom("babad") == "aba"
+assert Solution().longest_palindrom("cbbd") == "bb"
+assert Solution().longest_palindrom("aba") == "aba"
+assert Solution().longest_palindrom("ccc") == "ccc"
