acba47fabcb8c8e8dfeca93616ffda614d613f6a
Mike Czech <mike.czech@xing.com>
2022-05-03 19:14:58 +0200

    Add module

---
+++ kaggle/hm_fashion_reco/hm_fashion/model.py
@@ -0,0 +1,70 @@
+import argparse
+import os
+from typing import List
+from joblib import dump, load
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+
+import lightgbm
+import pandas as pd
+
+FEATURES = [
+    "feat_price_affinity",
+    "feat_dark_affinity",
+    "feat_dusty_light_affinity"
+]
+
+
+def select_columns(df: pd.DataFrame, cols: List[str]) -> pd.DataFrame:
+    """
+    Selects columns and returns a dataframe with only those.
+
+    Usually used with a FunctionTransformer.
+    """
+    return df[cols]
+
+
+def to_dataframe(X, col_labels: List[str]):
+    """
+    Creates a dataframe from an array and associates a label with each column.
+
+    This is mainly needed for scikit-learn's unfortunate support for pandas.
+    """
+    return pd.DataFrame(X, columns=col_labels)
+
+
+def get_pipeline(features: List[str]):
+    return Pipeline(
+        [
+            (
+                "to_dataframe",
+                FunctionTransformer(
+                    to_dataframe,
+                    kw_args={"col_labels": features},
+                ),
+            ),
+        ]
+    )
+
+
+def get_lgm_ranker_pipeline(
+    features=FEATURES,
+):
+    """
+    A pipeline based on LightGBM.
+    """
+    return Pipeline(
+        [
+            ("features", get_pipeline(features)),
+            (
+                "ranker",
+                lightgbm.LGBMRanker(
+                    objective="lambdarank",
+                    metric="ndcg",
+                )
+            ),
+        ]
+    )
+
