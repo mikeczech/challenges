f7825a31a56f0cc04c1bb1353e7a2825470519e2
Mike Czech <zenscr@gmail.com>
2023-12-23 10:59:05 +0100

    Add solution for AoC 2023, Day 3, Part 2

--- aoc/2023/day3/main.py
+++ aoc/2023/day3/main.py
@@ -1,13 +1,20 @@
 from typing import List
+from collections import defaultdict
 
-def main():
+def main_part_one():
     with open("input", "r") as f:
         schematic = f.read().splitlines()
-    sum_ = part_number_sum(schematic)
+    sum_ = part_number_sum_part_one(schematic)
+    print(sum_)
+
+def main_part_two():
+    with open("input", "r") as f:
+        schematic = f.read().splitlines()
+    sum_ = part_number_sum_part_two(schematic)
     print(sum_)
 
 
-def run_tests():
+def run_tests_part_one():
     schematic = [
         "467..114..",
         "...*......",
@@ -21,9 +28,28 @@ def run_tests():
         ".664.598.."
     ]
 
-    sum_ = part_number_sum(schematic)
+    sum_ = part_number_sum_part_one(schematic)
 
     assert sum_ == 4361
+
+
+def run_tests_part_two():
+    schematic = [
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598.."
+    ]
+
+    sum_ = part_number_sum_part_two(schematic)
+
+    assert sum_ == 467835
 
 
 def is_number(c: str):
@@ -36,7 +62,10 @@ def is_symbol(c: str):
         return False
     return True
 
-def part_number_sum(schematic: List[str]):
+def is_gear(c: str):
+    return c == "*"
+
+def part_number_sum_part_one(schematic: List[str]):
     ret = 0
     for i in range(len(schematic)):
         digits = []
@@ -67,5 +96,56 @@ def part_number_sum(schematic: List[str]):
     return ret
 
 
+def part_number_sum_part_two(schematic: List[str]):
+    gears = defaultdict(list)
+    for i in range(len(schematic)):
+        digits = []
+        is_adj_to_gears = set()
+        for j in range(len(schematic[i])):
+            if is_number(schematic[i][j]):
+                digits.append(schematic[i][j])
+
+                if ((i-1 >= 0) and is_gear(schematic[i-1][j])):
+                    is_adj_to_gears.add((i-1,j))
+
+                if ((i+1 < len(schematic)) and is_gear(schematic[i+1][j])):
+                    is_adj_to_gears.add((i+1,j))
+
+                if ((j-1 >= 0) and is_gear(schematic[i][j-1])):
+                    is_adj_to_gears.add((i,j-1))
+
+                if ((j+1 < len(schematic[i])) and is_gear(schematic[i][j+1])):
+                    is_adj_to_gears.add((i,j+1))
+
+                if ((i-1 >= 0) and (j-1 >= 0) and is_gear(schematic[i-1][j-1])):
+                    is_adj_to_gears.add((i-1,j-1))
+
+                if ((i+1 < len(schematic)) and (j+1 < len(schematic[i])) and is_gear(schematic[i+1][j+1])):
+                    is_adj_to_gears.add((i+1,j+1))
+
+                if ((j-1 >= 0) and (i+1 < len(schematic)) and is_gear(schematic[i+1][j-1])):
+                    is_adj_to_gears.add((i+1,j-1))
+
+                if ((i-1 >= 0) and (j+1 < len(schematic[i])) and is_gear(schematic[i-1][j+1])):
+                    is_adj_to_gears.add((i-1,j+1))
+            else:
+                if len(digits) > 0 and len(is_adj_to_gears) > 0:
+                    for gear in is_adj_to_gears:
+                        gears[gear].append(int("".join(digits)))
+                digits = []
+                is_adj_to_gears = set()
+
+
+        if len(digits) > 0 and len(is_adj_to_gears) > 0:
+            for gear in is_adj_to_gears:
+                gears[gear].append(int("".join(digits)))
+
+    ret = 0
+    for k, v in gears.items():
+        if len(v) == 2:
+            ret += v[0] * v[1]
+
+    return ret
+
 if __name__ == "__main__":
-    main()
+    main_part_two()
