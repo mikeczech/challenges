038e4cdf4359bf4f7d2faa3e7e4c24a36c4e96bd
Mike Czech <zenscr@gmail.com>
2023-12-24 10:36:44 +0100

    Add solution for AoC 2023 Day 4, Part 1

---
+++ aoc/2023/day4/main.py
@@ -0,0 +1,43 @@
+from typing import List
+
+def main():
+    with open("input", "r") as f:
+        cards = f.read().splitlines()
+    print(determine_points(cards))
+
+def run_tests():
+    cars = [
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+    ]
+
+    result = determine_points(cars)
+    assert result == 13
+
+def parse_card(card: str):
+    winning_numbers, your_numbers = card.split(":")[1].split("|")
+
+    winning_numbers = [int(x) for x in winning_numbers.strip().split(" ") if len(x) > 0]
+    your_numbers = [int(x) for x in your_numbers.strip().split(" ") if len(x) > 0]
+
+    return winning_numbers, your_numbers
+
+
+def determine_points(cards: List[str]):
+    ret = 0
+
+    for c in cards:
+        winning_numbers, your_numbers = parse_card(c)
+        num_matches = len(set(winning_numbers) & set(your_numbers))
+        if num_matches > 0:
+            ret += 2**(num_matches - 1)
+
+    return ret
+
+
+if __name__ == "__main__":
+    main()
